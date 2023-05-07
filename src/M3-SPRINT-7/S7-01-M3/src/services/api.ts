f5fb19fa9ab261ae2c5db0291936c7a3ea0dfec8
bitoller <bi_toller@hotmail.com>
2023-05-07 20:29:20 -0300

    change api address

--- src/M3-SPRINT-7/S7-01-M3/src/services/api.ts
+++ src/M3-SPRINT-7/S7-01-M3/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://furever-homes.onrender.com",
+  baseURL: "https://furever-homes-api.onrender.com",
   timeout: 8000,
 });
